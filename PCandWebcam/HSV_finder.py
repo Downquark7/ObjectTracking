f2f0071be9f10b5460c2adab4aca2475bd65110f
Downquark7 <mail2emn@gmail.com>
2016-05-03 17:17:58 -0500

    Update HSV_finder.py

--- PCandWebcam/HSV_finder.py
+++ PCandWebcam/HSV_finder.py
@@ -2,8 +2,8 @@ import numpy as np
 import cv2
 import time
 
-w=120
-h=160
+w=640
+h=480
 
 my_camera = cv2.VideoCapture(0)
 my_camera.set(3,w)
@@ -12,15 +12,16 @@ time.sleep(2)
 
 while (True):
     success, image = my_camera.read()
-    image = cv2.flip(image,-1)
+    h, w, channels = image.shape
+    #image = cv2.flip(image,-1)
     image = cv2.GaussianBlur(image,(5,5),0)
 
     image_HSV = cv2.cvtColor(image,cv2.COLOR_BGR2HSV)
     colour = str(image_HSV[h/2][w/2])
-    #cv2.line(image,(0,h/2),(w-1,h/2),[255,255,255])        
-    #cv2.line(image,(w/2,0),(w/2,h-1),[255,255,255])
-    #cv2.putText(image,colour,(10,30),cv2.FONT_HERSHEY_PLAIN,1,[255,255,255])
-    #cv2.imshow('View',image)
+    cv2.line(image,(0,h/2),(w-1,h/2),[255,255,255])        
+    cv2.line(image,(w/2,0),(w/2,h-1),[255,255,255])
+    cv2.putText(image,colour,(10,30),cv2.FONT_HERSHEY_PLAIN,1,[255,255,255])
+    cv2.imshow('View',image)
     print(colour)
     # Esc key to stop, otherwise repeat after 1 milliseconds
     key_pressed = cv2.waitKey(1)
